d2a0ab08512ba048d0011059ee156b5e99648b00
fatekey <32138014+fatekey@users.noreply.github.com>
2023-05-12 16:05:09 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -116,6 +116,12 @@ const BLOG = {
   MUSIC_PLAYER_ORDER: process.env.NEXT_PUBLIC_MUSIC_PLAYER_ORDER || 'list', // 默认播放方式，顺序 list，随机 random
   MUSIC_PLAYER_AUDIO_LIST: [ // 示例音乐列表。除了以下配置外，还可配置歌词，具体配置项看此文档 https://aplayer.js.org/#/zh-Hans/
     {
+      name: 'I’m still alive today',
+      artist: '96猫',
+      url: 'https://music.163.com/song/media/outer/url?id=1965919488.mp3',
+      cover: 'http://p1.music.126.net/R49UV9yz7DSC1Tz9s5i_xQ==/109951167701124811.jpg'
+    },
+    {
       name: 'I Really Want to Stay At Your House',
       artist: '銘晴Halo',
       url: 'https://music.163.com/song/media/outer/url?id=2003621098.mp3',
@@ -128,9 +134,9 @@ const BLOG = {
       cover: 'http://p2.music.126.net/juUqhD4PumKs5xfdgReZ7Q==/2528876746537453.jpg'
     }
   ],
-  MUSIC_PLAYER_METING: process.env.NEXT_PUBLIC_MUSIC_PLAYER_METING || false, // 是否要开启 MetingJS，从平台获取歌单。会覆盖自定义的 MUSIC_PLAYER_AUDIO_LIST，更多配置信息：https://github.com/metowolf/MetingJS
+  MUSIC_PLAYER_METING: process.env.NEXT_PUBLIC_MUSIC_PLAYER_METING || true, // 是否要开启 MetingJS，从平台获取歌单。会覆盖自定义的 MUSIC_PLAYER_AUDIO_LIST，更多配置信息：https://github.com/metowolf/MetingJS
   MUSIC_PLAYER_METING_SERVER: process.env.NEXT_PUBLIC_MUSIC_PLAYER_METING_SERVER || 'netease', // 音乐平台，[netease, tencent, kugou, xiami, baidu]
-  MUSIC_PLAYER_METING_ID: process.env.NEXT_PUBLIC_MUSIC_PLAYER_METING_ID || '60198', // 对应歌单的 id
+  MUSIC_PLAYER_METING_ID: process.env.NEXT_PUBLIC_MUSIC_PLAYER_METING_ID || '8413148463', // 对应歌单的 id
   MUSIC_PLAYER_METING_LRC_TYPE: process.env.NEXT_PUBLIC_MUSIC_PLAYER_METING_LRC_TYPE || '1', // 可选值： 3 | 1 | 0（0：禁用 lrc 歌词，1：lrc 格式的字符串，3：lrc 文件 url）
 
   // ----> 评论互动 可同时开启多个支持 WALINE VALINE GISCUS CUSDIS UTTERRANCES GITALK
